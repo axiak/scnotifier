3ce2712a06b8301a8ccefb2077c15963850cfd9e
Michael Axiak <mike@axiak.net>
2011-11-30 17:22:48 -0500

    Added first gm test.

--- web.js
+++ web.js
@@ -1,12 +1,30 @@
-var express = require('express');
+var temp = require('temp'),
+    sys = require('sys'),
+    fs = require('fs'),
+    gm = require('gm'),
+    http = require('http');
 
-var app = express.createServer(express.logger());
+function showError(res, description) {
+  res.writeHead(500, {"Content-Type": "text/plain"});
+  res.end(description);
+}
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+http.createServer(function (req, res) {
+  var tempFile = temp.path({suffix: ".png"});
+  gm(200, 400, "#ddff99f3")
+    .drawText(10, 50, "from scratch")
+    .write(tempFile, function (err) {
+      if (!err) {
+        fs.readFile(tempFile, function (err, data) {
+          if (!err) {
+            res.writeHead(200, {"Content-Type": "image/png"});
+            res.end(data);
+          } else {
+            showError(res, "Reading the temp file: " + err);
+          }
+        });
+      } else {
+        showError(res, "Writing the image: " + err);
+      }
+    });
+}).listen(5000, "127.0.0.1");
